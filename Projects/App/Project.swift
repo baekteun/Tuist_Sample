63e5800f21bfee1041d9208a24812931857f63bb
baegteun <baegteun@gmail.com>
2022-08-15 23:30:24 +0900

    :tada: ::

---
+++ Projects/App/Project.swift
@@ -0,0 +1,13 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+    name: "Sample",
+    platform: .iOS,
+    product: .app,
+    dependencies: [
+        .project(target: "Feature", path: .relativeToRoot("Projects/Feature"))
+    ],
+    resources: ["Resources/**"],
+    infoPlist: .file(path: "Support/Info.plist")
+)
